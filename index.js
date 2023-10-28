5ffb45a5faadb067163928fb40d08fafee15ead9
Nasir <nasirmia12901290@gmail.com>
2023-10-28 20:37:54 +0600

    add validation on user module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const { newUser } = require('./controller/userController');
 const app = express();
 dotenv.config();
 const port = process.env.PORT;
@@ -12,14 +13,11 @@ const router = express.Router();
 // middle wars 👇
 app.use(express.json());
 app.use(cors());
-app.use('/user', router)
+app.use('/', router)
 
 // methods 👇
 
-router.post('/', (req, res) => {
-    console.log('data from client', req.body)
-    res.json({ status: 'ok' })
-})
+router.post('/', newUser)
     .get('/', (req, res) => {
         res.json({ status: 'ok' })
     })
--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, match: /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/ },
+    name: {
+        type: String, required: true, validate: {
+            validator: function (v) {
+                return /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(v);
+            },
+            message: props => `${props.value} is not a valid name!!!!`
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!!!!!!`
+        }
+    },
     password: { type: String, required: true },
     qoute: { type: String }
 }
 )
 
 const User = mongoose.model('User', userSchema)
-module.exports= User;
+module.exports = User;
