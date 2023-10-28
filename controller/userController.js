2ede075bc0fb041d2582e94a52444c0630cbab9b
Nasir <nasirmia12901290@gmail.com>
2023-10-28 20:17:30 +0600

    add controller file

--- controller/userController.js
+++ controller/userController.js
@@ -2,8 +2,16 @@ const User = require("../models/userModel");
 
 
 
-const newUser = async (req,res) => {
+const newUser = async (req, res) => {
     const userInfo = req.body;
     const user = new User(userInfo)
-    user.save()
+
+    try {
+        const savedUser = await user.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        console.log('error occured while save new user :=> ', error)
+    }
 }
+
+module.exports={newUser};
