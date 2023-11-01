e8fe1fff6f14306a9342d9b7942fcf77bcc8417c
Nasir <nasirmia12901290@gmail.com>
2023-11-01 16:02:23 +0600

    add login functionality

--- controller/userController.js
+++ controller/userController.js
@@ -40,7 +40,7 @@ exports.login = async(req,res) => {
          }
     } catch (error) {
         
-        res.status(500).json({Error:"inte   rnal server error"})
+        res.status(500).json({Error:"inte  rnal server error"})
     }
        
 }
