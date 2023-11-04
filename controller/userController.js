2f06d72f2e29fd68a3726955ee96fd102fd4b71f
Nasir <nasirmia12901290@gmail.com>
2023-11-04 11:10:27 +0600

    add login functionality

--- controller/userController.js
+++ controller/userController.js
@@ -63,7 +63,7 @@ exports.login = async (req, res) => {
     }
 }
 exports.tokenVerification = (req, res, next) => {
-    const token = req.cookie.token;
+    const token = req.cookies.token;
     if (!token) {
         return res.json({ status: 'token error' })
     }
